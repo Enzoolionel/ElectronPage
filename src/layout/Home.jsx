8844e1c0cca54cf6dd208e0330d89f0e0cf2382a
enzoolionel <enzoperaltalio@gmail.com>
2024-10-22 09:37:13 -0300

    probando

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
 
-import MainSection from "./main";
-import ServiceSection from "./ServiceSection";
-import NavBar from "./NavBar";
-import MarcasSection from "./MarcasSection";
-import OpinionsSection from "./OpinionsSection";
-import Footer from "./Footer";
+import MainSection from "./main.jsx";
+import ServiceSection from "./ServiceSection.jsx";
+import NavBar from "./NavBar.jsx";
+import MarcasSection from "./MarcasSection.jsx";
+import OpinionsSection from "./OpinionsSection.jsx";
+import Footer from "./Footer.jsx";
 
 const Home = () => {
   return (
